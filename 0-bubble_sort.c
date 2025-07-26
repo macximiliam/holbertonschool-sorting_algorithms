781d58f6dc574acab889940ceb9fc00dc832bffe
macximiliam <alexanderbigman29@gmail.com>
2025-07-26 14:25:09 -0500

    Add functions of algorimths sortingbubble, selection, insertion and quick

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,14 @@
 #include "sort.h"
 
+/**
+ * bubble_sort - Sorts an array of integers using the bubble sort algorithm.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ *
+ * Description: This function sorts an array of integers in ascending order
+ * using the bubble sort algorithm. It repeatedly steps through the list,
+ * compares adjacent elements, and swaps them if they are in the wrong order.
+ */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
